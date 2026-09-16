db2a6524b6a1dec8334c223bfda0f3c19ba4dc83
Jonah Hofeld <jhofeld1@bu.edu>
2026-09-16 14:56:54 -0400

    Add our motto

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 const CREW_NAME: &str = "the unnamed crew";
 
 /// Your crew's motto. You will both change this one too, earlier and separately.
-const MOTTO: &str = "we have not agreed on a motto";
+const MOTTO: &str = "GSD";
 
 fn main() {
     println!("=== {} ===", CREW_NAME);
